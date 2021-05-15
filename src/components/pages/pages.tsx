f4b9626c84a3d994d5692a3c69eb0d7ce58b2409
Hari Chauhan <hari-c@hotmail.co.uk>
2021-05-15 23:05:53 +0100

    Add content

--- src/components/pages/pages.tsx
+++ src/components/pages/pages.tsx
@@ -1,35 +1,36 @@
-import { TweakContext } from "App";
-import { EmailIcon, GithubIcon, LinkedinIcon } from "components/icons/icons";
 import { Block } from "components/block/block";
 import { Content } from "components/content/content";
-import { DatGUIContext } from "components/datGUIWrapper/DatGUIWrapper";
-import React, { useContext } from "react";
-import { useLoader } from "react-three-fiber";
-import { state } from "store/store";
-import { TextureLoader } from "three";
 import { IconButton } from "components/icons/icon-button";
+import { EmailIcon, GithubIcon, LinkedinIcon } from "components/icons/icons";
+import React, { useContext } from "react";
+import FutropolisImage from "assets/futropolis.webp";
+import ClevryImage from "assets/clevry.webp";
+import LukeImage from "assets/luke-griffiths.webp";
+import GoogleTrustServicesImage from "assets/google-trust-services.webp";
+import { TweakContext } from "App";
 
 const colors = ["#B5D3DD", "#D9A59E", "#E3E37E", "#B5DFB6"];
 
 export const Pages: React.FC = () => {
   const linkToExternal = () => {};
+  const { color } = useContext(TweakContext);
   return (
     <>
       <Block factor={1} offset={0}>
         <Content side="left" className="portfolio-block" color={colors[0]}>
           <h1>Hi - I'm Hari </h1>
           <h2 style={{ color: "var(--blue-600)" }}>
-            A London based <span className="accent">software engineer</span>{" "}
-            with a focus on <span className="accent">front-end</span>.
+            A London-based <span className="accent">software engineer</span>{" "}
+            with a focus on <span className="accent">front-end</span>
           </h2>
           <h3>
             Outside of work you can find consuming excessive amount of music or
-            testing new recipies in the kitchen.
+            testing new recipies in the kitchen
           </h3>
           <h3 style={{ color: "var(--blue-600)" }}>
             Check out some of my favourite projects below. If you think we could
             work together{" "}
-            <span className="accent">feel free to get in touch!</span>
+            <span className="accent">feel free to get in touch</span>
           </h3>
           <div className="socials">
             <IconButton
@@ -51,13 +52,136 @@ export const Pages: React.FC = () => {
         </Content>
       </Block>
       <Block factor={-1} offset={1}>
-        <Content side="right" color={colors[1]}></Content>
+        <Content side="right" className="portfolio-block" color={colors[1]}>
+          <h1>Honeywell's Futropolis</h1>
+          <h2 style={{ color: "var(--blue-600)" }}>
+            A{" "}
+            <span className="accent" style={{ color }}>
+              modern 3D web experience
+            </span>{" "}
+            I built whilst at{" "}
+            <a
+              href="https://phantom.land"
+              target="_blank"
+              rel="noreferrer"
+              className="inline-link"
+            >
+              Phantom
+            </a>{" "}
+            built with <span className="accent">react-three-fiber</span> and{" "}
+            <span className="accent">Next.js</span>
+          </h2>
+          <a
+            className="image-container unstyled-button site-link"
+            href="https://futropolis.ft.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={FutropolisImage} alt="Futropolis" />
+          </a>
+        </Content>
       </Block>
       <Block factor={1} offset={2}>
-        <Content side="left" color={colors[2]}></Content>
+        <Content side="left" color={colors[2]} className="portfolio-block">
+          <h1>Lexii</h1>
+          <h2 style={{ color: "var(--blue-600)" }}>
+            A <span className="accent">language learning platform</span> aimed
+            at supporting refugee community integration. Ideated at the{" "}
+            <span className="accent">
+              <a
+                className="inline-link"
+                href="https://deendevelopers.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Deen Developers
+              </a>{" "}
+              x{" "}
+              <a
+                className="inline-link"
+                href="https://www.crisis.org.uk/get-involved/venture-studio/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Crisis Venture Studios
+              </a>{" "}
+            </span>
+            hackathon
+          </h2>
+          <h2>
+            Our site is still a work in progress, but you can check out our
+            pitch{" "}
+            <a
+              className="inline-link"
+              href="https://youtu.be/sfegAzGsFgg?t=3835"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+          </h2>
+        </Content>
       </Block>
       <Block factor={-1} offset={3}>
-        <Content side="right" color={colors[3]}></Content>
+        <Content side="right" color={colors[3]} className="portfolio-block">
+          <h1>Others</h1>
+          <div className="others">
+            <div className="other">
+              <a
+                className="image-container unstyled-button site-link other-site-link"
+                href="https://joy.clevry.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={ClevryImage} alt="Clevry" />
+              </a>
+              <p style={{ color: "var(--blue-600)" }}>
+                A{" "}
+                <span className="accent" style={{ color }}>
+                  psychometrics testing platform
+                </span>{" "}
+                I worked on whilst at{" "}
+                <a
+                  href="https://avamae.co.uk"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="inline-link"
+                >
+                  AVAMAE
+                </a>
+              </p>
+            </div>
+            <div className="other">
+              <a
+                className="image-container unstyled-button site-link other-site-link"
+                href="https://lukegriffiths.co.uk"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={LukeImage} alt="Luke Griffiths" />
+              </a>
+              <p style={{ color: "var(--blue-600)" }}>
+                A <span className="accent">portfolio site</span> for
+                London-based Graphic Designer{" "}
+                <span className="accent">Luke Griffiths</span>
+              </p>
+            </div>
+            <div className="other">
+              <a
+                className="image-container unstyled-button site-link other-site-link"
+                href="https://pki.goog"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={GoogleTrustServicesImage} alt="Clevry" />
+              </a>
+              <p style={{ color: "var(--blue-600)" }}>
+                <span className="accent">Google's PKI site</span>. Built at
+                Phantom and for which I was the sole developer
+              </p>
+            </div>
+          </div>
+        </Content>
       </Block>
     </>
   );
